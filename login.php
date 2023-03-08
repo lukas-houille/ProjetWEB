af6c48faf107deb0a5f1aa0229d533cba98ae917
Pow3y <lukas.houille@viacesi.fr>
2023-03-08 22:07:38 +0100

    Project organisation creation of login page

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page elements</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="connexion">
+    <form>
+        <img src="addons/images/LogoBig.svg">
+        <label for="username">
+            <span>Username</span>
+            <input type="text" name="username" id="username">
+        </label>
+        <label for="password">
+            <span>Password</span>
+            <input type="password" name="password" id="password">
+            <a href="http://google.com" target="_blank">Forgot password?</a>
+        </label>
+        <button type="submit">Sign in</button>
+    </form>
+</div>
+</body>
