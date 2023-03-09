54e6e4987fdaa8f316b5ae38c8585dc310011806
Pow3y <lukas.houille@viacesi.fr>
2023-03-09 12:03:20 +0100

    Footer base design

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="vertical-align">
+    <div class="horizontal-align" id="footer-content">
+
+        <img src="addons/images/LogoBig.svg" alt="logo">
+        <div class="horizontal-align" id="navigation">
+            <div class="vertical-align links">
+                <p>Navigation</p>
+                <a>cocucc</a>
+                <a>cocucc</a>
+                <a>cocucc</a>
+                <a>cocucc</a>
+            </div>
+            <div class="vertical-align links">
+                <a>cocucc</a>
+                <a>cocucc</a>
+                <a>cocucc</a>
+                <a>cocucc</a>
+            </div>
+        </div>
+    </div>
+    <div id="copyright">
+        <p>Copyright © 2022 - 2023 tous les droits réservés.</p>
+    </div>
+</footer>
