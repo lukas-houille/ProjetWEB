88d893f6b4b0631ab5b7ade167f9226b15188b01
Shulkogu <gabriel.anciaux@gmail.com>
2023-03-15 15:51:33 +0100

    cardoffer scss+php (without mustache php)

---
+++ cardoffer.php
@@ -0,0 +1,30 @@
+<?php
+include 'header.php';
+?>
+
+<div class = 'cardbody'>
+    <img src = 'resources/images/Logo small.svg' alt = 'logo test' class = 'logosmall'>
+    <div class = details>
+        <h2>Intitulé du poste</h2>
+        <p>Nom entreprise</p>
+        <p>Localisation</p>
+        <div class = 'description'>
+            <div>
+                <span>Profile</span>
+                <span>vartypeprofile</span>
+            </div>
+            <div>
+                <span>Durée</span>
+                <span>varduree</span>
+            </div>
+            <div>
+                <span>Publication</span>
+                <span>varpublication</span>
+            </div>
+            <div>
+                <span>Debut</span>
+                <span>vardebut</span>
+            </div>
+        </div>
+    </div>
+</div>
