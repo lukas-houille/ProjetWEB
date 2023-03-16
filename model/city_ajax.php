00b2c5423ba45dab71005e73809756726b381f06
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2023-03-16 17:05:43 +0100

    Added AJAX PHP City file
    Added AJAX PHP file for getting cities' names and IDs from the database in JavaScript.

---
+++ model/city_ajax.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'database.php';
+
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtoupper($_SERVER['HTTP_X_REQUESTED_WITH'])=="XMLHTTPREQUEST") {
+    if(isset($_POST["postcode"]) && preg_match("/(?=^.{0,5}$)[0-9]{5}/", $_POST["postcode"])) { // Check to see if the postcode sent is the right size
+        $result = $base->executeQuery("SELECT id_city, name FROM City WHERE postcode=:postcode", ["postcode" => $_POST["postcode"]]);
+        if(empty($result)) {
+            answer(404,["no cities found"]);
+        }
+        else {
+            answer(200,$result);
+        }
+    }
+    else {
+        answer(400,["postcode not sent or wrong format"]);
+    }
+}
+
+function answer(int $status_code,array $message) {
+    header("Content-Type: application/json");
+    http_response_code($status_code);
+    echo json_encode(["response_code" => $status_code, "message" => $message]);
+}
