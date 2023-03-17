ff8e77bcfc7d06814b38e25e05a819e00c57bb3c
Pow3y <lukas.houille@viacesi.fr>
2023-03-17 16:01:50 +0100

    Blocking access for none connected user on businesses and member page

--- business.php
+++ business.php
@@ -1,2 +1,9 @@
 <?php
-include "view/businesses-view.php";
+require_once "./model/session_model.php";
+initialise_session();
+if(check_login()) {
+    require_once "view/businesses-view.php";
+}
+else {
+    require_once "login.php";
+}
--- member.php
+++ member.php
@@ -1,3 +1,9 @@
 <?php
-
-require_once('view/member-view.php');
+require_once "./model/session_model.php";
+initialise_session();
+if(check_login()) {
+    require_once "view/member-view.php";
+}
+else {
+    require_once "login.php";
+}
