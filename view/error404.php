6bf5ef30ab82f5ca752059881f894bad8de609b1
Pow3y <lukas.houille@viacesi.fr>
2023-03-20 11:21:47 +0100

    404 error page

---
+++ view/error404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Erreur 404</title>
+    <?php
+    include('header.php');
+    ?>
+</head>
+<body>
+<link rel="stylesheet" href="error404.css">
+<form id="form">
+    <h1> Erreur 404 </h1>
+    <h2> Cette page n'existe pas</h2>
+    <h3>La prochaine fois essaye de prendre une boussole </h3>
+    <button type="submit" formaction="https://cesitonstage.com">Retrouver l'accueil</button>
+</form>
+</body>
+</html>
