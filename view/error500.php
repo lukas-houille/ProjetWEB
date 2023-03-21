795c77709987d7ba9145f48ee707095954c94caf
antonin <antoninbourrel76@gmail.com>
2023-03-21 14:02:37 +0100

    ajout page error 500

---
+++ view/error500.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Erreur 500</title>
+    <?php
+    include('header.php');
+    ?>
+</head>
+<body>
+<?php
+include('navbar.php');
+?>
+   <link rel="stylesheet" href="error500.css">
+<form id="form">
+<h1> Erreur 500 </h1>
+<h2> Serveur perdu ?</h2>
+<h3>Il a du oublié sa boussole </h3>
+
+<button type="button" id="url">Vidéo de chat pour attendre</button>
+
+</form>
+<?php
+include('footer.php');
+?>
+</body>
+</html>
