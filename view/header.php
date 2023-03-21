1b9abb47d76983deb68ba5674dcfa2f0a5d2573d
Pow3y <lukas.houille@viacesi.fr>
2023-03-21 08:59:28 +0100

    web manifest

--- view/header.php
+++ view/header.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="../style.css">
-<link rel="manifest" href="/manifest.json">
+<link rel="manifest" href="/site.webmanifest">
 <script type="text/javascript" src="resources/interaction.js"></script>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&D&display=swap" />
