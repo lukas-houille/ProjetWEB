e2761459fc206c507712b90a376591b82229f2d9
Shulkogu <gabriel.anciaux@gmail.com>
2023-03-22 11:49:03 +0100

    creation showcompany view php and showcompany style css

---
+++ view/showcompany-view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Company</title>
+    <script type="text/javascript" src="./resources/jquery.js"></script>
+    <script type="text/javascript" src="./resources/mustache.js"></script>
+    <link rel="stylesheet" href="showcompany.css">
+    <?php
+    include('header.php');
+    ?>
+</head>
+<body>
+<?php
+include('navbar.php');
+?>
+<div class="showcompanybody">
+
+    <div class="showdetailsbody">
+        <h1>{{company_name}}</h1>
+        <p>{{cities}}</p>
+        <p>Secteur d'activité:<span> {{field_name}}</span></p>
+        <p>Étudiants CESI déjà pris:<span> {{cesi_interns}}</span></p>
+        <div class="details">
+            <h1>Description de l'entreprise:</h1>
+            <p><span>{{description}}</span></p>
+        </div>
+        <div class="header-info">
+            <h1>Confiance des tuteurs:</h1>
+            <p><span>{{grade}}</span></p>
+        </div>
+        <div class="submit">
+            <button type="submit" formaction="https://cesitonstage.com/business.php">Retrouver la page entreprise</button>
+        </div>
+    </div>
+</div>
+
+<?php
+include('footer.html');
+?>
+</body>
+</html>
