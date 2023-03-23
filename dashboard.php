23a6b96c8c6082ad3ba45c432b8bab0cbb029eb7
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2023-03-23 15:13:06 +0100

    Added pagination for dashboards

--- dashboard.php
+++ dashboard.php
@@ -2,27 +2,36 @@
 require_once "./model/session_model.php";
 initialise_session();
 require_once "./model/dashboard_model.php";
+
+DEFINE("per_page",10); // Amount of maximum lines per page 
+
 if(isset($_SESSION["login"]) && $_SESSION["login"]->checkLogin()) {
+    if(isset($_GET["page"]) && !empty($_GET["page"]) && $_GET["page"]>0) {
+        $page = $_GET["page"];
+    }
+    else {
+        $page = 1;
+    }
     $type = ($_SESSION["login"]->isType($base));
     if(isset($_GET["view"]) && in_array($_GET["view"],["company","internship","tutor"])) {
         if($_GET["view"] == "company") {
-            $table = companyDashboard($base);
+            $table = companyDashboard($base, $page, per_page);
         }
         elseif($_GET["view"] == "internship") {
-            $table = internshipDashboard($base);
+            $table = internshipDashboard($base, $page, per_page);
         }
         elseif($_GET["view"] == "tutor") {
-            $table = tutorDashboard($base);
+            $table = tutorDashboard($base, $page, per_page);
         }
         require_once "view/dashboard-view.php";
     }
     else {
         if ($type[0] == "Admin" || $type[0] == "Tutor") {
             if ($type[0] == "Admin") {
-                $table = studentDashboardAdmin($base);
+                $table = studentDashboardAdmin($base, $page, per_page);
             }
             else {
-                $table = studentDashboardTutor($base, $type[1]);
+                $table = studentDashboardTutor($base, $type[1], $page, per_page);
             }
             require_once "view/dashboard-view.php";
         } else {
--- model/dashboard_model.php
+++ model/dashboard_model.php
@@ -4,27 +4,43 @@ require_once ("vendor/mustache/mustache/src/Mustache/Autoloader.php");
 Mustache_Autoloader::register();
 $m = new Mustache_Engine;
 
-function studentDashboardTutor(Database $base, int $id) {
-    global $m;
-    return($m->render(file_get_contents('view/templates-mustache/dashboard-table-student.mustache'), ["person" => $base->executeQuery("SELECT A.id_student,A.first_name,A.last_name,B.name AS center,A.name AS promotion FROM (SELECT Student.id_group,id_tutor,name,id_student,first_name,last_name,id_center FROM is_assigned_to JOIN Year_group ON is_assigned_to.id_group=Year_group.id_group JOIN Student ON Year_group.id_group=Student.id_group) AS A JOIN (SELECT id_tutor,first_name,last_name, Center.id_center, Center.name FROM Tutor JOIN Center ON Tutor.id_center=Center.id_center) AS B ON A.id_tutor=B.id_tutor AND A.id_center=B.id_center WHERE B.id_tutor=:id",["id" => $id])]));
+function studentDashboardTutor(Database $base, int $id, int $page, int $per_page) {
+    $table = $base->executeQuery("SELECT A.id_student,A.first_name,A.last_name,B.name AS center,A.name AS promotion FROM (SELECT Student.id_group,id_tutor,name,id_student,first_name,last_name,id_center FROM is_assigned_to JOIN Year_group ON is_assigned_to.id_group=Year_group.id_group JOIN Student ON Year_group.id_group=Student.id_group) AS A JOIN (SELECT id_tutor,first_name,last_name, Center.id_center, Center.name FROM Tutor JOIN Center ON Tutor.id_center=Center.id_center) AS B ON A.id_tutor=B.id_tutor AND A.id_center=B.id_center WHERE B.id_tutor=:id LIMIT :end OFFSET :beginning",["id" => $id, "end" => $per_page, "beginning" => ($per_page*($page-1))]);
+    $total = ceil($base->executeQuery("SELECT COUNT(A.id_student) FROM (SELECT Student.id_group,id_tutor,name,id_student,first_name,last_name,id_center FROM is_assigned_to JOIN Year_group ON is_assigned_to.id_group=Year_group.id_group JOIN Student ON Year_group.id_group=Student.id_group) AS A JOIN (SELECT id_tutor,first_name,last_name, Center.id_center, Center.name FROM Tutor JOIN Center ON Tutor.id_center=Center.id_center) AS B ON A.id_tutor=B.id_tutor AND A.id_center=B.id_center WHERE B.id_tutor=:id",["id" => $id], return_option:PDO::FETCH_NUM)[0][0]/$per_page);
+    return(applyTemplate($table, $page, $per_page, $total, "view/templates-mustache/dashboard-table-student.mustache"));
 }
 
-function studentDashboardAdmin(Database $base) {
-    global $m;
-    return($m->render(file_get_contents('view/templates-mustache/dashboard-table-student.mustache'), ["person" => $base->executeQuery("SELECT id_student,first_name,last_name,Center.name AS center,Year_group.name AS promotion FROM Student JOIN Center ON Student.id_center=Center.id_center JOIN Year_group ON Student.id_group=Year_group.id_group")]));
+function studentDashboardAdmin(Database $base, int $page, int $per_page) {
+    $table = $base->executeQuery("SELECT id_student,first_name,last_name,Center.name AS center,Year_group.name AS promotion FROM Student JOIN Center ON Student.id_center=Center.id_center JOIN Year_group ON Student.id_group=Year_group.id_group LIMIT :end OFFSET :beginning", ["end" => $per_page, "beginning" => ($per_page*($page-1))]);
+    $total = ceil($base->executeQuery("SELECT COUNT(id_student) FROM Student JOIN Center ON Student.id_center=Center.id_center JOIN Year_group ON Student.id_group=Year_group.id_group", return_option:PDO::FETCH_NUM)[0][0]/$per_page);
+    return(applyTemplate($table, $page, $per_page, $total, "view/templates-mustache/dashboard-table-student.mustache"));
 }
 
-function companyDashboard(Database $base) {
-    global $m;
-    return($m->render(file_get_contents('view/templates-mustache/dashboard-table-business.mustache'), ["company" => $base->executeQuery("SELECT Company.id_company,Company.name AS company_name,Company.cesi_interns,Company.email,Field.name AS field_name FROM Company JOIN Field ON Company.id_field=Field.id_field WHERE Company.visible=1")]));
+function companyDashboard(Database $base, int $page, int $per_page) {
+    $table = $base->executeQuery("SELECT Company.id_company,Company.name AS company_name,Company.cesi_interns,Company.email,Field.name AS field_name FROM Company JOIN Field ON Company.id_field=Field.id_field WHERE Company.visible=1 LIMIT :end OFFSET :beginning", ["end" => $per_page, "beginning" => ($per_page*($page-1))]);
+    $total = ceil($base->executeQuery("SELECT COUNT(Company.id_company) FROM Company JOIN Field ON Company.id_field=Field.id_field WHERE Company.visible=1", return_option:PDO::FETCH_NUM)[0][0]/$per_page);
+    return(applyTemplate($table, $page, $per_page, $total, "view/templates-mustache/dashboard-table-business.mustache"));
 }
 
-function internshipDashboard(Database $base) {
-    global $m;
-    return($m->render(file_get_contents('view/templates-mustache/dashboard-table-internship.mustache'), ["internship" => $base->executeQuery("SELECT Internship_offer.id_offer,duration,salary,date,places,description,Company.id_company,Company.name,City.name as city, postcode FROM Internship_offer JOIN Company ON Internship_offer.id_company=Company.id_company JOIN City ON Internship_offer.id_city=City.id_city WHERE Internship_offer.visible=1 AND Company.visible=1")]));
+function internshipDashboard(Database $base, int $page, int $per_page) {
+    $table = $base->executeQuery("SELECT Internship_offer.id_offer,duration,salary,date,places,description,Company.id_company,Company.name,City.name as city, postcode FROM Internship_offer JOIN Company ON Internship_offer.id_company=Company.id_company JOIN City ON Internship_offer.id_city=City.id_city WHERE Internship_offer.visible=1 AND Company.visible=1 LIMIT :end OFFSET :beginning", ["end" => $per_page, "beginning" => ($per_page*($page-1))]);
+    $total = ceil($base->executeQuery("SELECT COUNT(Internship_offer.id_offer) FROM Internship_offer JOIN Company ON Internship_offer.id_company=Company.id_company JOIN City ON Internship_offer.id_city=City.id_city WHERE Internship_offer.visible=1 AND Company.visible=1", return_option:PDO::FETCH_NUM)[0][0]/$per_page);
+    return(applyTemplate($table, $page, $per_page, $total, "view/templates-mustache/dashboard-table-internship.mustache"));
 }
 
-function tutorDashboard(Database $base) {
+function tutorDashboard(Database $base, int $page, int $per_page) {
+    $table = $base->executeQuery("SELECT id_tutor,first_name,last_name,name AS center FROM Tutor JOIN Center ON Center.id_center=Tutor.id_center LIMIT :end OFFSET :beginning", ["end" => $per_page, "beginning" => ($per_page*($page-1))]);
+    $total = ceil($base->executeQuery("SELECT COUNT(id_tutor) FROM Tutor JOIN Center ON Center.id_center=Tutor.id_center", return_option:PDO::FETCH_NUM)[0][0]/$per_page);
+    return(applyTemplate($table, $page, $per_page, $total, "view/templates-mustache/dashboard-table-tutor.mustache"));
+}
+
+function applyTemplate(array $table, int $page, int $per_page, int $total, string $template_location) {
     global $m;
-    return($m->render(file_get_contents('view/templates-mustache/dashboard-table-tutor.mustache'), ["person" => $base->executeQuery("SELECT id_tutor,first_name,last_name,name AS center FROM Tutor JOIN Center ON Center.id_center=Tutor.id_center")]));
+    if(($page+1) > $total) {
+        $next=$page;
+    }
+    else {
+        $next=$page+1;
+    }
+    return($m->render(file_get_contents($template_location), ["iteration" => $table, "previous_page" => $page-1, "next_page" => $next, "page" => $page, "total_page" => $total]));
 }
