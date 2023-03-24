437d67b7afe5520c3a7e2b93600642e4904da68d
antonin <antoninbourrel76@gmail.com>
2023-03-24 01:28:14 +0100

    rgpd

---
+++ view/rgpd-view.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Politique de confidentialité | Cesi Ton Stage</title>
+  <?php
+    include('header.php');
+    ?>
+   <link rel="stylesheet" href="rgpd.css">
+</head>
+
+<body>
+    <?php
+include('navbar.php');
+
+?>
+
+<div id="mentionslegale ">
+    
+     
+
+
+<form>
+  <h1>Politique de confidentialité du site</h1>
+
+  <p>Chez Cesi Ton Stage, nous sommes soucieux et attachés au respect de la vie privée de nos utilisateurs. Nous sommes conscients de l'importance de protéger les données personnelles de nos utilisateurs et de respecter les lois et réglementations en vigueur. Nous nous engageons à respecter leur vie privée conformément à la législation en vigueur sur la protection des données, notamment le Règlement Général sur la Protection des Données (RGPD) et politique de l'école CESI.</p>
+
+  <h2>Collecte et utilisation des données</h2>
+
+  <p>Les informations collectées lors de l'utilisation de notre site sont utilisées dans le seul but de permettre aux étudiants de consulter les offres de stage proposées par les entreprises partenaires de Cesi Ton Stage. Nous ne collectons aucune donnée personnelle sans l'accord de nos utilisateurs. Les seules données personnelles collectées sont celles fournies volontairement lors de la création d'un compte utilisateur.</p>
+
+  <p>Les données collectées sont les suivantes : identifiants et mot de passe, données des entreprises demandeur de stage. Ces données sont utilisées pour permettre l'accès à l'interface utilisateur de notre site et pour recevoir des notifications de nouvelles offres de stage. Les données personnelles collectées ne sont ni vendues ni transmises à des tiers.</p>
+
+  <h2>Protection des données personnelles</h2>
+
+  <p>Chez Cesi Ton Stage, nous prenons toutes les mesures nécessaires pour assurer la sécurité et la confidentialité des données personnelles de nos utilisateurs. Les accès aux données personnelles sont strictement réservés aux administrateurs du site ainsi que certaines aux pilotes de formations. Nous nous engageons à mettre en place toutes les mesures nécessaires pour empêcher la divulgation, la perte, l'altération ou la destruction de vos données personnelles.</p>
+
+  <h2>Liens vers des sites tiers</h2>
+
+  <p>Notre site peut contenir des liens vers des sites tiers. Nous ne sommes pas responsables des pratiques de ces sites tiers en matière de collecte et d'utilisation de données personnelles. Nous vous invitons à lire attentivement les politiques de confidentialité de ces sites tiers.</p>
+
+  <h2>Modification de la politique de confidentialité</h2>
+
+  <p>Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Nous vous encourageons à consulter régulièrement cette page pour prendre connaissance des éventuelles mises à jour.</p>
+
+  <h2>Contact</h2>
+
+  <p>Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité, n'hésitez pas à nous contacter à l'adresse e-mail suivante : <a class="lienmail" href="mailto:adresse@email-de-lentreprise">adresse@email-de-lentreprise</a>.</p>
+  <p> deuxiéme option mettre le liens vers contact et rajouter en service concérné </p>
+  <form>
+  <?php include('footer.php'); ?>
+  
+</body>
+
+</html>
