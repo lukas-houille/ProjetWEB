ef8885e7c48f354863a4c5175bb82b14b08cdb77
Pow3y <lukas.houille@viacesi.fr>
2023-03-27 11:26:04 +0200

    Antonin partial code correction

--- view/mentionslegal-view.php
+++ view/mentionslegal-view.php
@@ -4,50 +4,51 @@
     <title>Mentions légal</title>
     <?php
     include('header.php');
-?>
-  
-	<link rel="stylesheet" href="mentionslegal.css">
+    ?>
+
+    <link rel="stylesheet" href="resources/styles/mentionslegal.css">
 <body>
 <?php
 require_once('view/navbar-view.php');
 ?>
-<div id="mentionslegale ">
-    
-       
-
-
-
-<main>
+<div id="mentionslegale" class="content">
         <h1>Mentions légales</h1>
-		<h2>Éditeur du site</h2>
-    <p>Le présent site est édité par :</p>
-    <ul>
-        <li>Raison sociale : Cesi Ton Stage</li>
-        <li>Adresse : 6 rue du Bois Chênes le Loup</li>
-        <li>Téléphone : +33 1 23 45 67 89</li>
-        <li>Email :<a class="lienrgpd" href="mailto:contact@pulseheberg.com">Envoyer un e-mail</e-mail></a> </li>
-        <li>Immatriculée au RCS de Nancy sous le numéro 123 456 789</li>
-        <li>Numéro SIRET : 123 456 789 00012</li>
-        <li>Capital social :  100 000 euros</li>
-        <li>Directeur de la publication : Antonin Bourrel</li>
-        <li>Hébergeur : PulseHeberg, 9 Boulevard de Strasbourg, 83000 Toulon <a class="lienrgpd" href="mailto:contact@pulseheberg.com">Envoyer un e-mail</e-mail></a> <a class="lienhebergeur" href="tel:+33422141360">+33 (0) 4 22 14 13 60 </a>
-</li>
-    </ul>
+        <h2>Éditeur du site</h2>
+        <p>Le présent site est édité par :</p>
+        <ul>
+            <li>Raison sociale : Cesi Ton Stage</li>
+            <li>Adresse : 6 rue du Bois Chênes le Loup</li>
+            <li>Téléphone : +33 1 23 45 67 89</li>
+            <li>Email :<a class="lienrgpd" href="mailto:contact@pulseheberg.com">Envoyer un e-mail</e-mail></a></li>
+            <li>Immatriculée au RCS de Nancy sous le numéro 123 456 789</li>
+            <li>Numéro SIRET : 123 456 789 00012</li>
+            <li>Capital social : 100 000 euros</li>
+            <li>Directeur de la publication : Antonin Bourrel</li>
+            <li>Hébergeur : PulseHeberg, 9 Boulevard de Strasbourg, 83000 Toulon <a class="lienrgpd"
+                                                                                    href="mailto:contact@pulseheberg.com">Envoyer
+                    un e-mail</e-mail></a> <a class="lienhebergeur" href="tel:+33422141360">+33 (0) 4 22 14 13 60 </a>
+            </li>
+        </ul>
 
-    <h2>Protection des données personnelles</h2>
-    <p>Le traitement de vos données à caractère personnel est régi par notre Charte de protection des données personnelles, disponible depuis la section "Protection des données personnelles", conformément au Règlement Général sur la Protection des Données 2016/679 du 27 avril 2016 (« RGPD »)<a class="lienhebergeur" href="rgpd.php">RGPD<a></p>
+        <h2>Protection des données personnelles</h2>
+        <p>Le traitement de vos données à caractère personnel est régi par notre Charte de protection des données
+            personnelles, disponible depuis la section "Protection des données personnelles", conformément au Règlement
+            Général sur la Protection des Données 2016/679 du 27 avril 2016 (« RGPD »)<a class="lienhebergeur"
+                                                                                         href="rgpd.php">RGPD<a></p>
 
-    <h2>Conditions d'utilisation</h2>
-    <p>En utilisant notre site, vous acceptez nos conditions d'utilisation, disponibles depuis la section "Conditions d'utilisation".</p>
+        <h2>Conditions d'utilisation</h2>
+        <p>En utilisant notre site, vous acceptez nos conditions d'utilisation, disponibles depuis la section
+            "Conditions d'utilisation".</p>
 
-    <h2>Propriété intellectuelle</h2>
-    <p>L'ensemble du contenu de ce site (textes, logos, images, etc.) est protégé par le droit d'auteur et autres droits de propriété intellectuelle. Toute reproduction ou utilisation non autorisée du contenu du site est interdite.</p>
-</main>
+        <h2>Propriété intellectuelle</h2>
+        <p>L'ensemble du contenu de ce site (textes, logos, images, etc.) est protégé par le droit d'auteur et autres
+            droits de propriété intellectuelle. Toute reproduction ou utilisation non autorisée du contenu du site est
+            interdite.</p>
 </div>
 
 
 <?php
-require_once('view/footer.html');
+require_once('view/footer-view.html');
 ?>
 </body>
 </html>
