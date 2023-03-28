d7bf3307e8f43a03537002f974976d6f549c6726
Pow3y <lukas.houille@viacesi.fr>
2023-03-28 23:53:57 +0200

    fav offer + var dump

--- member.php
+++ member.php
@@ -19,9 +19,10 @@ if(isset($_SESSION["login"]) && $_SESSION["login"]->checkLogin()) {
         $data = $db->executeQuery("SELECT description, Company.name as companyName, City.name as location FROM Internship_offer INNER JOIN Company ON Internship_offer.id_company=Company.id_company INNER JOIN City ON Internship_offer.id_city=City.id_city INNER JOIN wishes ON wishes.id_offer=Internship_offer.id_offer INNER JOIN Student ON Student.id_student=Wishes.id_student WHERE Student.id_student = :id;",["id" => $id],return_option: PDO::FETCH_OBJ);
     } elseif($type[0] == "Admin"){
         $id = $db->executeQuery("SELECT id_admin FROM Admin WHERE login = :login",["login" => $_SESSION["login"]->GetLogin() ],return_option: PDO::FETCH_OBJ);
-        $data = $db->executeQuery("SELECT description, Company.name as companyName, City.name as location FROM Internship_offer INNER JOIN Company ON Internship_offer.id_company=Company.id_company INNER JOIN City ON Internship_offer.id_city=City.id_city INNER JOIN admin_wishes ON admin_wishes.id_offer=Internship_offer.id_offer INNER JOIN Admin ON Admin.id_admin=admin_wishes.id_admin WHERE Admin.id_Admin = :id;",["id" => $id],return_option: PDO::FETCH_OBJ);
+        $data = $db->executeQuery("SELECT description, Company.name as companyName, City.name as location FROM Internship_offer INNER JOIN Company ON Internship_offer.id_company=Company.id_company INNER JOIN City ON Internship_offer.id_city=City.id_city INNER JOIN admin_wishes ON admin_wishes.id_offer=Internship_offer.id_offer INNER JOIN Admin ON Admin.id_admin=admin_wishes.id_admin WHERE Admin.id_admin = :id;",["id" => $id],return_option: PDO::FETCH_OBJ);
     }
     // show all the cards in favorite with mustache template
+    $id;
     var_dump($data);
     $cards = "";
     Mustache_Autoloader::register();
