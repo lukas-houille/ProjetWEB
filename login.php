fad357d707a3694bfddb7b686da901d0e902dffe
Pow3y <lukas.houille@viacesi.fr>
2023-03-28 14:15:27 +0200

    Session storing

--- login.php
+++ login.php
@@ -4,8 +4,6 @@ $warning = "";
 if((isset($_POST["username"]) && !empty($_POST["username"])) && (isset($_POST["password"]) && !empty($_POST["password"]))) {
     $login = new Login($_POST["username"]);
     if ($login->checkPassword($_POST["password"])) {
-        setcookie("user_login", $_POST["username"], time() + 3600, "/");
-        setcookie("user_password", $_POST["password"], time() + 3600, "/");
         initialise_session();
         $_SESSION["login"] = new Session($_POST["username"]);
         if(basename($_SERVER["SCRIPT_FILENAME"]) == "login.php") {
