a1e22eef282dcd9ed685075c68cbac10a1452eb9
Pow3y <lukas.houille@viacesi.fr>
2023-03-28 23:47:35 +0200

    fav offer + var dump

--- member.php
+++ member.php
@@ -16,10 +16,10 @@ if(isset($_SESSION["login"]) && $_SESSION["login"]->checkLogin()) {
     // get id student from session
     if ($type[0] == "Student") {
         $id = $db->executeQuery("SELECT id_student FROM student WHERE login = :login",["login" => $_SESSION["login"]->GetLogin() ],return_option: PDO::FETCH_OBJ);
-        $data = $db->executeQuery("SELECT description, company.name as companyName, city.name as location FROM internship_offer INNER JOIN company ON internship_offer.id_company=company.id_company INNER JOIN city ON internship_offer.id_city=city.id_city INNER JOIN wishes ON wishes.id_offer=internship_offer.id_offer INNER JOIN Student ON Student.id_student=Wishes.id_student WHERE Student.id_student = :id;",["id" => $id],return_option: PDO::FETCH_OBJ);
+        $data = $db->executeQuery("SELECT description, Company.name as companyName, City.name as location FROM Internship_offer INNER JOIN Company ON Internship_offer.id_company=Company.id_company INNER JOIN City ON Internship_offer.id_city=City.id_city INNER JOIN wishes ON wishes.id_offer=Internship_offer.id_offer INNER JOIN Student ON Student.id_student=Wishes.id_student WHERE Student.id_student = :id;",["id" => $id],return_option: PDO::FETCH_OBJ);
     } elseif($type[0] == "Admin"){
         $id = $db->executeQuery("SELECT id_Admin FROM student WHERE login = :login",["login" => $_SESSION["login"]->GetLogin() ],return_option: PDO::FETCH_OBJ);
-        $data = $db->executeQuery("SELECT description, company.name as companyName, city.name as location FROM internship_offer INNER JOIN company ON internship_offer.id_company=company.id_company INNER JOIN city ON internship_offer.id_city=city.id_city INNER JOIN admin_wishes ON admin_wishes.id_offer=internship_offer.id_offer INNER JOIN Admin ON Admin.id_admin=admin_wishes.id_admin WHERE Admin.id_Admin = :id;",["id" => $id],return_option: PDO::FETCH_OBJ);
+        $data = $db->executeQuery("SELECT description, Company.name as companyName, City.name as location FROM Internship_offer INNER JOIN Company ON Internship_offer.id_company=Company.id_company INNER JOIN City ON Internship_offer.id_city=City.id_city INNER JOIN admin_wishes ON admin_wishes.id_offer=Internship_offer.id_offer INNER JOIN Admin ON Admin.id_admin=admin_wishes.id_admin WHERE Admin.id_Admin = :id;",["id" => $id],return_option: PDO::FETCH_OBJ);
     }
     // show all the cards in favorite with mustache template
     var_dump($data);
