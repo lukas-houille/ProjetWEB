9a56dfd2dc63a59d3dbd76e9d7965bbd9ec55cda
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2023-03-28 17:51:08 +0200

    Fixed wrong titles

--- view/offers-view.php
+++ view/offers-view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Offers</title>
+    <title>Offres</title>
     <script type="text/javascript" src="./resources/jquery.js"></script>
     <script type="text/javascript" src="./resources/mustache.js"></script>
     <link rel="stylesheet" type="text/css" href="resources/styles/offers.css">
