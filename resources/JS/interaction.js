c7c7b44295936d22979d1fcb07f2f0f3662330db
Pow3y <lukas.houille@viacesi.fr>
2023-03-29 09:06:45 +0200

    js event listener error handler

--- resources/JS/interaction.js
+++ resources/JS/interaction.js
@@ -1,7 +1,6 @@
 window.onload = function() {
     const allPopups = document.getElementsByClassName("popup");
-    const btnClose = document.getElementsByClassName("popup-close")[0];
-    btnClose?.addEventListener('click', function hidePopUp() { allPopups[0].style.display = "none"; }, false);
+    document.getElementsByClassName("popup-close")[0]?.addEventListener('click', function hidePopUp() { allPopups[0].style.display = "none"; }, false);
 
     openPopUp = function (id) {
         document.getElementById(id).style.display = "block";
